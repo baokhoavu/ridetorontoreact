f74c500ca98ae87fc11c16b13c02c96b0dba4f86
Bao Vu <baovu@Baos-MacBook-Pro.local>
2019-10-03 13:17:14 -0700

    created footer need icons for socials

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 
 const Footer = () => (
 	<div class="footer">		
-		<div class="container-fluid">
+		<div class="container-fluid row">
 			<div class="col-md-3 first">
 				
 				<a href="#">
@@ -34,14 +34,14 @@ const Footer = () => (
 			</div>
 			<div class="col-md-2">
 				<p>
-					<span><a href="#"><strong>Dollars At Work</strong></a></span>
+					<span class="heavy"><strong><a href="#">Dollars At Work</a></strong></span>
 					<br />
-					<span><a href="#"><strong>Cancer In Canada</strong></a></span>
+					<span class="heavy"><strong><a href="#">Cancer In Canada</a></strong></span>
 					<br />
-					<span><a href="#"><strong>Tour the Pmcc</strong></a></span>
+					<span class="heavy"><strong><a href="#">Tour the Pmcc</a></strong></span>
 				</p>
 				<p>
-					<span><a href="#"><strong>Ride</strong></a></span>
+					<span class="heavy"><strong><a href="#">Ride</a></strong></span>
 					<br />
 					<span><a href="#">About the ride</a></span>
 					<br />
@@ -54,7 +54,7 @@ const Footer = () => (
 					<span><a href="#">How We Support You</a></span>
 				</p>
 	            <p>
-					<span><a href="#"><strong>Partners</strong></a></span>
+					<span class="heavy"><strong><a href="#">Partners</a></strong></span>
 					<br />
 					<span><a href="#">Our Partners</a></span>
 					<br />
@@ -62,13 +62,69 @@ const Footer = () => (
 				</p>
 	        </div>
 	        <div class="col-md-2">
-			
+				<p>
+					<span class="heavy"><strong><a href="#">News</a></strong></span>
+					<br />
+					<span class="heavy"><strong><a href="#">Photos</a></strong></span>
+					<br />
+					<span class="heavy"><strong><a href="#">Videos</a></strong></span>
+				</p>
+				<p>
+					<span class="heavy"><strong><a href="#">Riders</a></strong></span>
+					<br />
+					<span><a href="#">Top Fundraisers</a></span>
+					<br />
+					<span><a href="#">Ambassadors</a></span>
+					<br />
+					<span><a href="#">Polka Dot Jerseys</a></span>
+					<br />
+					<span><a href="#">5 and 10 Year Riders</a></span>
+					<br />
+					<span><a href="#">Riders' Stories</a></span>
+				</p>
+	            <p>
+					<span class="heavy"><strong><a href="#">Teams</a></strong></span>
+					<br />
+					<span><a href="#">Top Teams</a></span>
+					<br />
+					<span><a href="#">Ride As A Team</a></span>
+					<br />
+					<span><a href="#">Industry Challenge</a></span>
+					<br />
+					<span><a href="#">Community Challenge</a></span>
+				</p>
 			</div>
 	        <div class="col-md-2">
-	            {/*<img class="partners" src="https://ride.conquercancer.ca/toronto20/wp-content/uploads/2019/06/livepatrol_940x400.png" />*/}
+	            <p>
+					<span class="heavy"><strong><a href="#">Crew & Volunteer</a></strong></span>
+					<br />
+					<span><a href="#">How You Can Help</a></span>
+					<br />
+					<span><a href="#">Important Info</a></span>
+					<br />
+					<span><a href="#">Join As A Group</a></span>
+					<br />
+					<span><a href="#">Our Support</a></span>
+				</p>
+	            <p>
+					<span class="heavy"><strong><a href="#">Fundraising</a></strong></span>
+					<br />
+					<span><a href="#">Toolkit</a></span>
+					<br />
+					<span><a href="#">Refer and Raise</a></span>
+					<br />
+					<span><a href="#">Peloton Bike Giveaway</a></span>
+					<br />
+					<span><a href="#">Ultimate Camp Happy Hour</a></span>
+				</p>
+				<p>
+					<span class="heavy"><strong><a href="#">Faqs</a></strong></span>
+					<br />
+					<span class="heavy"><strong><a href="#">Privacy Policy</a></strong></span>
+				</p>
 	        </div>
 	        <div class="col-md-3">
-	        
+	        	© 2019 CauseForce, LLC. The Ride to Conquer Cancer and Princess Margaret Cancer Centre names and logos are registered trademarks of The Princess Margaret Cancer Foundation, used under license. Enbridge is a registered trademark of Enbridge, Inc., used under license.
 	        </div>
 		</div>
 	</div>
