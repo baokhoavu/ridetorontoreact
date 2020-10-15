ceef8e968fbd6f079da12f1c206cf4df112bffc0
Khoa <tealkrysta92@gmail.com>
2020-10-14 22:58:40 -0700

    creating base

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -312,7 +312,7 @@ export default class Header extends Component {
 	render() {
 		return (
 			<div >		
-				<Router>
+				<Router basename={'/ridetorontoreact'}>
 
 					{/* Head Div */}
 					<div className="header">
